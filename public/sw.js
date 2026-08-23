ba2250d82ab5e374e383c37fd3ebbb4791fe40fb
mortalsking <rushikeshbardeskar28@gmail.com>
2026-08-23 23:50:25 +0530

    chore: scaffold Vite + React + TypeScript project

---
+++ public/sw.js
@@ -0,0 +1,48 @@
+const CACHE = "lifeutils-v2";
+const SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+
+  if (req.mode === "navigate") {
+    e.respondWith(
+      fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put("/index.html", copy));
+          return res;
+        })
+        .catch(() => caches.match("/index.html")),
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(req).then(
+      (hit) =>
+        hit ??
+        fetch(req).then((res) => {
+          if (res.ok && new URL(req.url).origin === self.location.origin) {
+            const copy = res.clone();
+            caches.open(CACHE).then((c) => c.put(req, copy));
+          }
+          return res;
+        }),
+    ),
+  );
+});
